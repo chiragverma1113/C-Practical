5a15b8bb52d579c7cea07685e8bcbc033a75ec2b
Chirag Verma <chiragverma1113@gmail.com>
2021-01-06 12:00:06 +0530

    Update 13- Grade According to Marks.c

--- 13- Grade According to Marks.c
+++ 13- Grade According to Marks.c
@@ -14,25 +14,15 @@ int main()
 	avg=(sub1+sub2+sub3)/3;
 	printf("\nAverage Marks : %d\n",avg);	
 	if(avg>=80 && avg<=100)
-	{
 	        printf("\nGrade : Honors");
-	}
 	else if(avg>=60 && avg<=79)
-	{
 	        printf("\nGrade : First Division");
-	}
 	else if(avg>=50 && avg<=59)
-	{
 	        printf("\nGrade : Second Division");
-	}
 	else if(avg>=40 && avg<=49)
-	{
-	        printf("\nGrade : Third Division");
-	}		
+	        printf("\nGrade : Third Division");		
 	else if(avg>=0 && avg<=39)
-	{
-	        printf("\nGrade : Fail");
-	}	
+	        printf("\nGrade : Fail");	
     else;
     printf("\n");
 	getch();
