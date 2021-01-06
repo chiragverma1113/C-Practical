0cc251713e9f570971d496ab9d0dc69598c59a08
Chirag Verma <chiragverma1113@gmail.com>
2021-01-06 12:51:10 +0530

    Update 15- Prime or Not Prime.c

--- 15- Prime or Not Prime.c
+++ 15- Prime or Not Prime.c
@@ -9,23 +9,17 @@ int main()
   for(i=2;i<=num/2;i++)
   {
     if(num%i==0)
-    {
       count=1;
-    }
   }
   if(count==0)
-  {
     printf("The given number %d is a Prime Number\n",num);
-  }
   else
   {
     printf("The given number %d is not a Prime Number\nBecause The number is divisible by\n",num);
     for(i=1;i<=num;i++)
     {
       if(num%i==0)
-      {
         printf("%d\n",i);
-      }
     }
   }
 }
